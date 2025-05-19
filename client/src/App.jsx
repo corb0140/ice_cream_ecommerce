2ef9395573dfd6d5bb507249d616465dcdc64809
corb0140 <corb0140@algonquinlive.com>
2025-05-19 12:35:02 -0400

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import AppRouter from "./router/routes";
+
+function App() {
+  return (
+    <div className="h-screen">
+      <AppRouter />
+    </div>
+  );
+}
+
+export default App;
---
+++ server/src/server.js
@@ -0,0 +1,22 @@
+const dotenv = require("dotenv");
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const logger = require("./helpers/logger");
+
+// CONFIGURATIONS
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+
+// IMPORT ROUTES
+
+// ROUTES
+
+// SERVER
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  logger.info(`Server is running on port ${PORT}`);
+});
