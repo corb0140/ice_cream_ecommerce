a9f9f62ffee5ad15cb2155a38234cc6b597c5157
corb0140 <corb0140@algonquinlive.com>
2025-06-08 13:59:17 -0400

    uplaod image middleware added

--- server/src/middleware/uploadImage.js
+++ server/src/middleware/uploadImage.js
@@ -3,3 +3,5 @@ const multer = require("multer");
 const uploadImage = multer({
   storage: multer.memoryStorage(),
 });
+
+module.exports = uploadImage.single("image"); // 'image' is the field name in the form data
