38d53efdad4e8b2eb09b15f420974f3cff36e92a
corb0140 <corb0140@algonquinlive.com>
2025-07-09 18:21:50 -0400

    testing vercel

--- server/src/server.js
+++ server/src/server.js
@@ -9,7 +9,22 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+
+// CORS CONFIGURATION
+const allowedOrigins = [process.env.CLIENT_URL, process.env.VERCEL_URL];
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
+// app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 
 // IMPORT ROUTES
 const authRoutes = require("./routes/authRoutes");
